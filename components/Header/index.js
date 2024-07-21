a77f75973c696b28d9bb2a277c8c8487e9b5d3a1
firoz3130 <firosk7@gmail.com>
2024-07-21 14:26:45 +0530

    Data modfiued

--- components/Header/index.js
+++ components/Header/index.js
@@ -174,7 +174,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:firosk7@gmail.com")}>
               Contact
             </Button>
 
