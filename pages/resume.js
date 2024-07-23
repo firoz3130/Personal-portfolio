2db75324eacd92069674a3efe2e01d133a34321f
firoz3130 <firosk7@gmail.com>
2024-07-23 23:26:24 +0530

    Add Link to resume

--- pages/resume.js
+++ pages/resume.js
@@ -33,12 +33,20 @@ const Resume = () => {
       >
         <Header isBlog />
         {mount && (
+   
           <div className="mt-10 w-full flex flex-col items-center">
-            <div
-              className={`w-full ${
+           <div className="mt-5">
+    <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-20 py-2.5 text-center me-2 mb-2">Link to Resume</button>
+</div>
+
+
+           
+            <div 
+              className={`mt-5 w-full ${
                 mount && theme.theme === "dark" ? "bg-slate-800" : "bg-gray-50"
               } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-sm`}
             >
+
               <h1 className="text-3xl font-bold">{name}</h1>
               <h2 className="text-xl mt-5">{resume.tagline}</h2>
               <h2 className="w-4/5 text-xl mt-5 opacity-50">
