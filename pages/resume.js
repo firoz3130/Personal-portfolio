52e53382f5fb2450f7344801e4770e8e85de4e5f
firoz3130 <firosk7@gmail.com>
2024-07-24 11:16:10 +0530

    Link to Resume

--- pages/resume.js
+++ pages/resume.js
@@ -36,8 +36,11 @@ const Resume = () => {
    
           <div className="mt-10 w-full flex flex-col items-center">
            <div className="mt-5">
-    <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-20 py-2.5 text-center me-2 mb-2">Link to Resume</button>
+    <a href={resume.linkToResume} target="_blank" rel="noopener noreferrer" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-20 py-2.5 text-center inline-block">
+        Link to Resume
+    </a>
 </div>
+
 
 
            
